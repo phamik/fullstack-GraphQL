ded7b4be259beb79bbd86812660d68a63c47a96b
Tien Pham <tmpham95@gmail.com>
2020-07-19 18:32:46 +0100

    adding client app to this repo

---
+++ client-react-apollo/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import App from './App';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+  uri: 'https://7sgx4.sse.codesandbox.io'
+})
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
